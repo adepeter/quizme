944e78de34131bbd89318ece191e4e8ded356df7
Oluwaseun Peter <adepeter26@gmail.com>
2020-08-28 23:44:01 +0100

    Initial commit

--- quizme/settings.py
+++ quizme/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'jeae7gh+_c*r$ys&)7k6q6^n#vj)*or&kq@_(1#e=-=1537@19'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
