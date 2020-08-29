6776a4a54483bc6306d70bd6b2179b17f685fe4b
Oluwaseun Peter <adepeter26@gmail.com>
2020-08-29 12:47:55 +0100

    Initial commit

--- quizme/settings.py
+++ quizme/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-# STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static'
+# ]
+STATIC_ROOT = BASE_DIR / 'static'
 
 CACHES = {
     'default': {
